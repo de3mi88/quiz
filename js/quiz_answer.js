6c728418dbdfd4745b4d10bbcd87b37c15fe6e8c
de3mi88 <vladimirturbabin@gmail.com>
2022-11-07 18:50:02 +0300

    make new quiz

---
+++ js/quiz_answer.js
@@ -0,0 +1,11 @@
+let answer = {
+    "s" : {
+        "description" : "Вы - Тони старк"
+    },
+    "w" : {
+        "description" : "Вы - Черный Негр"
+    },
+    "c" : {
+        "description" : "Вы - Капитан Залупа"
+    },
+};
